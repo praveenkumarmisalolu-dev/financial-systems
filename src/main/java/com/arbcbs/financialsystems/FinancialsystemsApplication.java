49e8acbff257b5c414e7d6ddc993e7b07975aeba
praveen_kumar_misalolu <25737998+praveenkumarmisalolu-dev@users.noreply.github.com>
2024-11-08 11:32:09 +0530

    First Commit

---
+++ src/main/java/com/arbcbs/financialsystems/FinancialsystemsApplication.java
@@ -0,0 +1,13 @@
+package com.arbcbs.financialsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialsystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialsystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arbcbs/financialsystems/FinancialsystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbcbs.financialsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialsystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
